dbd099e33fd6a8216531fd7cf8785d72622d07a6
boublo <david53940@gmail.com>
2024-05-05 23:28:47 +0200

    add page reservation

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -8,6 +8,7 @@ export const allRoutes = [
     new Route("/signup", "Inscription", "/pages/auth/signup.html"),
     new Route("/account", "Mon compte", "/pages/auth/account.html"),
     new Route("/editpassword", "Mon compte", "/pages/auth/editpassword.html"),
+    new Route("/allresa", "Vos reservation", "/pages/reservations/allresa.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
